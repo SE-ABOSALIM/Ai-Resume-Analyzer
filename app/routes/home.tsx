384410881e46f6fcc082c27b2ec6cd7c7bfef03c
SE-ABOSALIM <muhammedchreiki@stu.topkapi.edu.tr>
2025-10-08 14:11:50 +0300

    Start Piont (Setup Styles and Resorces)

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Analyze Resume" },
+    { name: "description", content: "Best Resume, Dream Job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <section className="main-selection">
+        <div className="page-heading">
+          <h1>Analyze, Rank, and Perfect Your Resume with AI</h1>
+          <h2>AI-powered analysis to refine and improve your resumes.</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
